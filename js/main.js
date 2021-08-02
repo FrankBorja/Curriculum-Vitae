fc17e399f82e343def510c87f9c3cc47317bc637
Frank <frankborja03@gmail.com>
2021-08-02 15:09:14 -0500

    Menu terminado

--- js/main.js
+++ js/main.js
@@ -28,7 +28,31 @@
       document.querySelector(".fade-out-effect").classList.remove("active");
     },300)
   }
-
+  // attach an event handler to document
+  document.addEventListener("click", (event) =>{
+    if(event.target.classList.contains('link-item')){
+        /* make sure event.target.hash has a value before overridding default behavior */
+      if(event.target.hash !==""){
+        // prevent default anchor click behavior
+        event.preventDefault();
+        const hash = event.target.hash;
+        // deactivate existing active 'section' 
+        document.querySelector(".section.active").classList.add("hide");
+        document.querySelector(".section.active").classList.remove("active");
+        // activate new 'section' 
+        document.querySelector(hash).classList.add("active");
+        document.querySelector(hash).classList.remove("hide");
+        /* deactivate existing active navigation menu 'link-item' */
+        navMenu.querySelector(".active").classList.add("outer-shadow","hover-in-shadow");
+        navMenu.querySelector(".active").classList.remove("active","inner-shadow");
+        // activate new navigation menu 'link-item'
+        event.target.classList.add("active","inner-shadow");
+        event.target.classList.remove("outer-shadow","hover-in-shadow");
+        // hide navigation menu
+        hideNavMenu();
+      }
+    }
+  })
 })();
 
 /*--------------- about section tabs ---------------*/
@@ -217,7 +241,7 @@ function bodyScrollingToggle(){
 /*--------------- hide sections except active ---------------*/ 
 
 (() =>{
-
+  
   const sections = document.querySelectorAll(".section");
   sections.forEach((section) =>{
     if(!section.classList.contains("active")){
